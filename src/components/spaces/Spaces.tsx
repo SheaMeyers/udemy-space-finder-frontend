ad38c14faadc63f95b078701b82bf9bcd11ea59f
Shea Meyers <shea.meyers@gmail.com>
2021-08-07 21:33:55 +0200

    Add reservation confirmation modal

--- src/components/spaces/Spaces.tsx
+++ src/components/spaces/Spaces.tsx
@@ -2,10 +2,13 @@ import { Component } from "react";
 import { Space } from "../../model/Model";
 import { DataService } from "../../services/DataService";
 import { SpaceComponent } from "./SpaceComponent";
+import { ConfirmModalComponent } from './ConfirmModalComponent';
 
 
 interface SpacesState {
-    spaces: Space[]
+    spaces: Space[],
+    showModal: boolean,
+    modalContent: string
 }
 
 interface SpacesProps {
@@ -18,9 +21,12 @@ export class Spaces extends Component<SpacesProps, SpacesState> {
         constructor(props: SpacesProps) {
             super(props)
             this.state = {
-                spaces: []
+                spaces: [],
+                showModal: false,
+                modalContent: ''
             }
             this.reserveSpace = this.reserveSpace.bind(this);
+            this.closeModal = this.closeModal.bind(this);
         }
 
         async componentDidMount(){
@@ -28,7 +34,20 @@ export class Spaces extends Component<SpacesProps, SpacesState> {
             this.setState({ spaces })
         }
 
-        private async reserveSpace(spaceId: string){}
+        private async reserveSpace(spaceId: string){
+            const reserverationResult = await this.props.dataService.reserveSpace(spaceId)
+            if (reserverationResult) {
+                this.setState({
+                    showModal: true,
+                    modalContent: `You reserved the space with id ${spaceId} and got the reservtion number ${reserverationResult}`
+                })
+            } else {
+                this.setState({
+                    showModal: true,
+                    modalContent: `You can not reserve the space with spaceId ${spaceId}`
+                })
+            }
+        }
 
         private renderSpaces(){
             const rows: any[] = []
@@ -45,10 +64,22 @@ export class Spaces extends Component<SpacesProps, SpacesState> {
             return rows
         }
 
+        private closeModal() {
+            this.setState({
+                showModal: false,
+                modalContent: ''
+            })
+        }
+
         render() {
             return <div>
                 <h2>Welcome to the Spaces page!</h2>
                 {this.renderSpaces()}
+                <ConfirmModalComponent 
+                    close={this.closeModal}
+                    content={this.state.modalContent}
+                    show={this.state.showModal}
+                />
             </div>
         }
 }
