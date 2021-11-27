dad6c25e62ab020756d62e0b3ec3d00dde94e1f7
Shea Meyers <shea.meyers@gmail.com>
2021-11-27 17:18:58 +0100

    Mock authentication service to further test login

--- test/components/Login.test.tsx
+++ test/components/Login.test.tsx
@@ -1,14 +1,28 @@
 import ReactDOM from 'react-dom';
 import { Login } from '../../src/components/Login';
-import { fireEvent } from '@testing-library/react';
+import { fireEvent, waitFor } from '@testing-library/react';
+import { User } from '../../src/model/Model';
+import history from '../../src/utils/history';
+
+
+const someUser: User = {
+    userName: 'someUser',
+    email: 'someEmail'
+}
+
 
 describe('Login component test suite', () => {
 
     let container: HTMLDivElement;
+    
     const authServiceMock = {
         login: jest.fn()
     }
+    
     const setUserMock = jest.fn();
+
+    const historyMock = history;
+    history.push = jest.fn();
 
     beforeEach(() => {
         container = document.createElement('div');
@@ -53,5 +67,41 @@ describe('Login component test suite', () => {
         fireEvent.click(loginButton);
 
         expect(authServiceMock.login).toBeCalledWith('someUser', 'somePassword');
+    })
+
+    test('Correctly handles successful login', async () => {
+        authServiceMock.login.mockResolvedValueOnce(someUser);
+
+        const inputs = document.querySelectorAll('input');
+        const usernameInput = inputs[0];
+        const passwordInput = inputs[1];
+        const loginButton = inputs[2];
+
+        fireEvent.change(usernameInput, {target: {value: 'someUser'}});
+        fireEvent.change(passwordInput, {target: {value: 'somePassword'}});
+        fireEvent.click(loginButton);
+
+        const statusLabel = await waitFor(() => container.querySelector('label'));
+        expect(statusLabel).toBeInTheDocument();
+        expect(statusLabel).toHaveTextContent('Login successful');
+        expect(setUserMock).toBeCalledWith(someUser);
+        expect(historyMock.push).toBeCalledWith('/profile');
+    })
+
+    test('Correctly handles unsuccessful login', async () => {
+        authServiceMock.login.mockResolvedValueOnce(undefined);
+
+        const inputs = document.querySelectorAll('input');
+        const usernameInput = inputs[0];
+        const passwordInput = inputs[1];
+        const loginButton = inputs[2];
+
+        fireEvent.change(usernameInput, {target: {value: 'someUser'}});
+        fireEvent.change(passwordInput, {target: {value: 'somePassword'}});
+        fireEvent.click(loginButton);
+
+        const statusLabel = await waitFor(() => container.querySelector('label'));
+        expect(statusLabel).toBeInTheDocument();
+        expect(statusLabel).toHaveTextContent('Login failed');
     })
 })
