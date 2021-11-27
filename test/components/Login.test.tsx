67d8c7041394eff0b766d4a8d0a6efa5d4d8c22e
Shea Meyers <shea.meyers@gmail.com>
2021-11-27 16:39:38 +0100

    Set up basic test for checking rendering

---
+++ test/components/Login.test.tsx
@@ -0,0 +1,43 @@
+import ReactDOM from 'react-dom';
+import { Login } from '../../src/components/Login';
+
+describe('Login component test suite', () => {
+
+    let container: HTMLDivElement;
+    const authServiceMock = {
+        login: jest.fn()
+    }
+    const setUserMock = jest.fn();
+
+    beforeEach(() => {
+        container = document.createElement('div');
+        document.body.appendChild(container);
+        ReactDOM.render(
+            <Login 
+                authService={authServiceMock as any} 
+                setUser={setUserMock}
+            />,
+            container
+        )
+    })
+
+    afterEach(() => {
+        document.body.removeChild(container);
+        container.remove();
+        jest.clearAllMocks();
+    })
+
+    test('Renders initial correcty', () => {
+        const title = document.querySelector('h2');
+        expect(title!.textContent).toBe('Please login');
+
+        const inputs = document.querySelectorAll('input');
+        expect(inputs).toHaveLength(3);
+        expect(inputs[0].value).toBe('');
+        expect(inputs[1].value).toBe('');
+        expect(inputs[2].value).toBe('Login');
+
+        const label = document.querySelector('label');
+        expect(label).not.toBeInTheDocument();
+    })
+})
